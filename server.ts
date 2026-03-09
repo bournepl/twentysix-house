7bc0f8f2ca320691dfffedcd134fd2b0985683b4
workpoint-news <workpointnews01@gmail.com>
2026-03-09 15:59:08 +0700

    fix ssr deploy

--- server.ts
+++ server.ts
@@ -2,16 +2,15 @@ import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
+import { dirname, join } from 'node:path';
 import AppServerModule from './src/main.server';
-import { routes } from './src/app/app-routing.module';
 
-// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
+
   const server = express();
+
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const distFolder = resolve(serverDistFolder, '..');
-  const browserDistFolder = resolve(distFolder, 'browser');
+  const browserDistFolder = join(serverDistFolder, '../browser');
 
   const indexHtml = join(browserDistFolder, 'index.html');
 
@@ -20,68 +19,31 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
-  server.get('/sitemap.xml', (req, res) => {
-    const baseUrl = 'https://twentysix.house';
-
-    const flattenRoutes = (routes: any[], parent: string = ''): string[] => {
-      let urls: string[] = [];
-      for (const route of routes) {
-        const fullPath = `${parent}/${route.path}`.replace(/\/+/g, '/');
-        if (route.data?.sitemap !== false && route.path !== '**') {
-          urls.push(`${baseUrl}${fullPath === '/' ? '' : fullPath}`);
-        }
-        if (route.children) {
-          urls = urls.concat(flattenRoutes(route.children, fullPath));
-        }
-      }
-      return urls;
-    };
-
-    const urls = flattenRoutes(routes);
-    const sitemapXml = `<?xml version="1.0" encoding="UTF-8" ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(url => `  <url><loc>${url}</loc></url>`).join('\n')}
-</urlset>`;
-
-    res.header('Content-Type', 'application/xml');
-    res.send(sitemapXml);
-  });
-
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
+  // serve static files
   server.use(express.static(browserDistFolder, {
     maxAge: '1y'
   }));
 
   // SSR
   server.get('*', (req, res, next) => {
+
     const { protocol, originalUrl, baseUrl, headers } = req;
 
-    commonEngine
-      .render({
-        bootstrap: AppServerModule,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+    commonEngine.render({
+      bootstrap: AppServerModule,
+      documentFilePath: indexHtml,
+      url: `${protocol}://${headers.host}${originalUrl}`,
+      publicPath: browserDistFolder,
+      providers: [
+        { provide: APP_BASE_HREF, useValue: baseUrl }
+      ]
+    })
+      .then(html => res.send(html))
+      .catch(err => next(err));
+
   });
 
   return server;
 }
 
-function run(): void {
-  const port = process.env['PORT'] || 4000;
-
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
-
-//run();
 export default app();
